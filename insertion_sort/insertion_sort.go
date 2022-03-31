2d4534c99fc332fe0eb7a024c69df0b5f250be30
martynocchio <martynocchio@mail.ru>
2022-03-31 20:56:18 +0300

    data_structure/

---
+++ insertion_sort/insertion_sort.go
@@ -0,0 +1,12 @@
+package main
+
+func selectionSort(arr []int) []int {
+	for i := 0; i < len(arr); i++ {
+		for j := 0; j < i; j++ {
+			if arr[j] > arr[i] {
+				arr[j], arr[i] = arr[i], arr[j]
+			}
+		}
+	}
+	return arr
+}
